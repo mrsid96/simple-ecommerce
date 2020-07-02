40e1c461578cea7699f796e8c26323ff77d39c8d
Sidharth Patnaik <sidharth@signzy.com>
2020-07-02 13:37:02 +0530

    Added withRouter to menuItem component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import "./App.css"
-import {HomePage} from './components/Homepage/homepage.component'
+import { HomePage } from "./pages/Homepage/homepage.component"
+import { Switch, Route } from 'react-router-dom';
+
+const HatsPage = () => {
+  return (<div>
+    <h1>Hats Page</h1>
+  </div>);
+}
 
 function App() {
   return (
     <div>
-      <HomePage/>
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/hats" component={HatsPage} />
+      </Switch>
     </div>
   );
 }
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,19 +1,22 @@
 import React from "react";
 import "./menu-item.styles.scss";
+import { withRouter } from "react-router-dom";
 
-export const MenuItem = ({ title, imageUrl, size }) => {
-  return (
-    <div className={`${size} menu-item`}>
-      <div
-        className="background-image"
-        style={{
-          backgroundImage: `url(${imageUrl})`,
-        }}
-      />
-      <div className="content">
-        <h1 className="title">{title.toUpperCase()}</h1>
-        <span className="subtitle">Show now</span>
+export const MenuItem = withRouter(
+  ({ title, imageUrl, size, history, match, linkUrl }) => {
+    return (
+      <div className={`${size} menu-item`} onClick={() => history.push(`${match.url}${linkUrl}`)}>
+        <div
+          className="background-image"
+          style={{
+            backgroundImage: `url(${imageUrl})`,
+          }}
+        />
+        <div className="content">
+          <h1 className="title">{title.toUpperCase()}</h1>
+          <span className="subtitle">Show now</span>
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+);
