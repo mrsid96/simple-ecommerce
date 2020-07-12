393ddc6d3c7a54cbe3673d1f4d5b6abf7ff0a0cb
Sidharth Patnaik <sidharth@signzy.com>
2020-07-12 20:58:43 +0530

    modified cart utils

--- src/redux/cart/cart.util.js
+++ src/redux/cart/cart.util.js
@@ -1,47 +1,22 @@
-const addItemToCart = (itemList, newItem) => {
-    let itemMatch = false;
-    for (let i = 0; i < itemList.length; i++) {
-        if (itemList[i].id === newItem.id) {
-            itemMatch = true;
-            itemList[i]["quantity"] = itemList[i]["quantity"] + 1
-        }
-    }
-    return itemMatch ? [...itemList] : [...itemList, { ...newItem, quantity: 1 }];
-}
-
 const clearItems = (itemList, removeItem) => {
     return [...itemList.filter(item => removeItem.id !== item.id)];
 }
 
-// const removeItem = (itemList, removeItem) => {
-//     let singleItem = false;
-//     for (let i = 0; i < itemList.length; i++) {
-//         if (itemList[i].id === removeItem.id) {
-//             if (itemList[i]["quantity"] - 1 > 0) {
-//                 itemList[i]["quantity"] = itemList[i]["quantity"] - 1
-//             } else {
-//                 singleItem = true;
-//             }
-//         }
-//     }
-//     return singleItem ? clearItems(itemList, removeItem) : [...itemList];
-// }
+const addItemToCart = (cartItems, cartItemToAdd) => {
+    const existingCartItem = cartItems.find(
+        cartItem => cartItem.id === cartItemToAdd.id
+    );
 
-// const addItemToCart = (cartItems, cartItemToAdd) => {
-//     const existingCartItem = cartItems.find(
-//         cartItem => cartItem.id === cartItemToAdd.id
-//     );
+    if (existingCartItem) {
+        return cartItems.map(cartItem =>
+            cartItem.id === cartItemToAdd.id
+                ? { ...cartItem, quantity: cartItem.quantity + 1 }
+                : cartItem
+        );
+    }
 
-//     if (existingCartItem) {
-//         return cartItems.map(cartItem =>
-//             cartItem.id === cartItemToAdd.id
-//                 ? { ...cartItem, quantity: cartItem.quantity + 1 }
-//                 : cartItem
-//         );
-//     }
-
-//     return [...cartItems, { ...cartItemToAdd, quantity: 1 }];
-// };
+    return [...cartItems, { ...cartItemToAdd, quantity: 1 }];
+};
 
 const removeItem = (cartItems, cartItemToRemove) => {
     const existingCartItem = cartItems.find(
